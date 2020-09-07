561704ed3d10acf20a67b11479c3a05be6a28c47
joyinmyzone <jxyzsyxx17@hotmail.com>
2020-09-07 13:47:06 -0700

    2020/9/7 lab cont.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+/**
+ * The enter-point of a dart App
+ */
 void main() {
+  // runApp takes a Widget
+  // Flutter framework expands and displays to the screen at run time.
   runApp(
+    // The MaterialApp widget becomes the root of your app's widget tree.
     MaterialApp(
       title: 'FriendlyChat',
+      // The home argument specifies the default screen that users see in your app.
       home: Scaffold(
         appBar: AppBar(
           title: Text('FriendlyChat'),
